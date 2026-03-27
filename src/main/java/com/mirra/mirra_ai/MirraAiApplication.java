00f7d9ecce13928124a53479dcf45d094f4bb294
Aditya Gole <adityagole123@gmail.com>
2026-03-28 05:12:24 +0530

    initial commit

---
+++ src/main/java/com/mirra/mirra_ai/MirraAiApplication.java
@@ -0,0 +1,13 @@
+package com.mirra.mirra_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirraAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirraAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirra/mirra_ai/MirraAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirra.mirra_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirraAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
